c7710b8e6b640fdb7082c61e9fad78b4bcf8c92e
Andrey Drozd <bestspacemail@gmail.com>
2022-05-23 09:27:06 +0300

    some changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,135 +3,139 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
-const isDevelopment = process.env.NODE_ENV === 'development'
+module.exports = (env = {}) => {
+    const { mode = 'development' } = env
+    const isDev = mode === 'development'
 
-module.exports = {
-// режим приложения
-mode: isDevelopment ? 'development' : 'production',
+    return {
+        // режим приложения
+        mode: isDev ? 'development' : 'production',
 
-// модули (функции)
-module: {
-    rules: [
-        {
-            test: /\.(png|jpe?g|gif|svg)$/i,
-            exclude: /node_modules/,
-            type: 'asset/resource',
-            use: [
+        // модули (функции)
+        module: {
+            rules: [
                 {
-                    loader: 'file-loader',
-                    options: {
-                        outputPath: 'images',
-                        name: '[name]-[sha1:hash:7].[ext]',
-                        esModule: false
-                    },
-                },
-            ],
-            type: 'javascript/auto'
-        },
-         {
-             test: /\.(ttf|otf|eot|woff|woff2)$/i,
-             exclude: /node_modules/,
-             type: 'asset/resource',
-             use: [
-                 {
-                     loader: 'file-loader',
-                     options: {
-                         outputPath: 'fonts',
-                         name: '[name].[ext]',
-                     },
-                 },
-             ],
-         },
-        {
-            test: /\.(js|jsx)$/i,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
-                    },
-                },
-            ],
-        },
-        {
-            test: /\.(sc|c)ss$/i,
-            exclude: /node_modules/,
-            use: [
-                isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader', // лоадер для преобразования CSS - в модуль JavaScript
-                    options: {
-                        modules: false, // для задания модульности css (уникальные стили)
-                    }
-                },
-                {
-                    loader: "postcss-loader",
-                    options: {
-                        postcssOptions: {
-                            plugins: [
-                                [
-                                    "postcss-preset-env", {}
-                                ]
-                            ],
+                    test: /\.(png|jpe?g|gif|svg|ico)$/i,
+                    exclude: /node_modules/,
+                    type: 'asset/resource',
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'images',
+                                name: '[name]-[sha1:hash:8].[ext]',
+                                esModule: false
+                            },
                         },
-                    },
+                    ],
+                    type: 'javascript/auto'
                 },
                 {
-                    loader: 'sass-loader'
+                    test: /\.(ttf|otf|eot|woff|woff2)$/i,
+                    exclude: /node_modules/,
+                    type: 'asset/resource',
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                outputPath: 'fonts',
+                                name: '[name].[ext]',
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(js|jsx)$/i,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                            options: {
+                                plugins: [isDev && require.resolve('react-refresh/babel')].filter(Boolean),
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.(sc|c)ss$/i,
+                    exclude: /node_modules/,
+                    use: [
+                        isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader', // лоадер для преобразования CSS - в модуль JavaScript
+                            options: {
+                                modules: false, // для задания модульности css (уникальные стили)
+                            }
+                        },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                postcssOptions: {
+                                    plugins: [
+                                        [
+                                            "postcss-preset-env", {}
+                                        ]
+                                    ],
+                                },
+                            },
+                        },
+                        {
+                            loader: 'sass-loader'
+                        },
+                    ]
                 },
             ]
         },
-    ]
-},
 
-// путь в entry файл[ы]
-entry: {
-    main: ("./src/index.js"),
-},
+        // путь в entry файл[ы]
+        entry: {
+            main: ("./src/index.js"),
+        },
 
-// путь для бандл файла
-output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: isDevelopment ? '[name].js' : '[name].[contenthash].js',
-    clean: true
-},
+        // путь для бандл файла
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: isDev ? '[name].js' : '[name].[hash:8].js',
+            clean: true
+        },
 
-// карты
-devtool: isDevelopment ? 'source-map' : false,
+        // карты
+        devtool: isDev ? 'source-map' : false,
 
-// оптимизация импортируемых библиотек (отдельный бандл с импортами для переиспользования)
-optimization: {
-    splitChunks:{ chunks: 'all' }
-},
+        // оптимизация импортируемых библиотек (отдельный бандл с импортами для переиспользования)
+        optimization: {
+            splitChunks:{ chunks: 'all' }
+        },
 
-// чтобы не указывать импорт с расширеням
-// можно дополнить указав alias, для задания импортов типа import Utility from 'Utilities/utility'
-resolve: {
-    extensions: ['*', '.js', '.jsx', '.css', '.scss']
-},
+        // чтобы не указывать импорт с расширеням
+        // можно дополнить указав alias, для задания импортов типа import Utility from 'Utilities/utility'
+        resolve: {
+            extensions: ['*', '.js', '.jsx', '.css', '.scss']
+        },
 
-// плагины (классы)
-plugins: [
-    // создаст основной html и свяжет билд JS
-    new HtmlWebPackPlugin({
-        buildTime: new Date().toISOString(),
-        template: './src/index.html',
-        favicon: './public/favicon.ico'
-    }),
+        // плагины (классы)
+        plugins: [
+            // создаст основной html и свяжет билд JS
+            new HtmlWebPackPlugin({
+                title: 'no CRA',
+                favicon: './public/favicon.ico',
+                template: './public/index.html',
+            }),
 
-    // извлекат css в отдельные файлы и добавляет ссылки на эти файлы в HTML документ (для мастера)
-    new MiniCssExtractPlugin({
-        filename: isDevelopment ? '[name].css' : '[name].[contenthash].css'
-    }),
+            // извлекат css в отдельные файлы и добавляет ссылки на эти файлы в HTML документ (для мастера)
+            new MiniCssExtractPlugin({
+                filename: isDev ? '[name].css' : '[name].[hash:8].css'
+            }),
 
-    // горячяя перезагрузка
-    isDevelopment && new ReactRefreshWebpackPlugin()
-].filter(Boolean),
+            // горячяя перезагрузка
+            isDev && new ReactRefreshWebpackPlugin()
+        ].filter(Boolean),
 
-// настройки сервера для разработки
-devServer: {
-    port: 3000,
-    hot: true,
-},
+        // настройки сервера для разработки
+        devServer: {
+            hot: true,
+            open: true,
+            port: 3000,
+        },
+    }
 }
